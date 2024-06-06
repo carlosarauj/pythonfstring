3062f12e0c4110a1126a8c1cff4c34be5a325b77
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-06-06 19:44:39 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,11 @@
+v1 = 'Abc'
+
+print(f'{v1}')
+print(f'{v1:>10}')
+print(f'{v1:<10}')
+print(f'{v1:^10}')
+print(f'{v1:a^10}')
+print(f'{1241241.341431:.2f}')
+
+#python py.py
+#ctrl ; para comentar
